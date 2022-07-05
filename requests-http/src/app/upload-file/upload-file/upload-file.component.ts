8a8079c531c81b5fc368653823664abce2345c63
Victor Martins <victor_martins_castro@hotmail.com>
2022-07-05 15:02:26 -0300

    refactoring da progressbar

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,6 +1,8 @@
+import { environment } from './../../../environments/environment';
 import { UploadFileService } from './../upload-file.service';
 import { Component, OnInit } from '@angular/core';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs-operators';
 
 @Component({
   selector: 'app-upload-file',
@@ -37,20 +39,28 @@ export class UploadFileComponent implements OnInit {
   onUpload() {
     if (this.files && this.files.size > 0) {
       this.service
-        .upload(this.files, '/api/upload')
-        .subscribe((event: HttpEvent<Object>) => {
+        .upload(this.files, environment.BASE_URL + 'upload')
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress);
+            this.progress = progress;
+          }),
+          filterResponse()
+        )
+        .subscribe(response => console.log('Upload Concluido'));
+        /* .subscribe((event: HttpEvent<Object>) => {
           // HttpEventType
-          console.log(event);
+          //console.log(event);
           if (event.type === HttpEventType.Response) {
             console.log('upload concluido');
           } else if (event.type === HttpEventType.UploadProgress) {
             const percentDone = Math.round(
               (event.loaded * 100) / (event.total || 1)
             );
-            console.log('Progresso', percentDone);
+            //console.log('Progresso', percentDone);
             this.progress = percentDone;
           }
-        });
+        }); */
     }
   }
 }
