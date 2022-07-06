afa7279b097b44f577f082b0ab10f8ef6aa021f9
Victor Martins <victor_martins_castro@hotmail.com>
2022-07-06 08:30:57 -0300

    finalizando a busca reativa

--- requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, tap, map } from 'rxjs';
+import { Observable, tap, map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
@@ -11,13 +11,30 @@ import { FormControl } from '@angular/forms';
 export class LibSearchComponent implements OnInit {
   queryField = new FormControl();
   readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  readonly FIELDS = 'name,description,version,homepage';
 
   results$!: Observable<any>;
   total!: number;
 
   constructor(private _http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.results$ = this.queryField.valueChanges
+      .pipe(
+        map(value => value.trim()),
+        filter(value => value.length > 1),
+        debounceTime(400),
+        distinctUntilChanged(),
+        switchMap( value => this._http.get(this.SEARCH_URL, {
+          params: {
+            search: value,
+            fields: this.FIELDS
+          }
+        })),
+        tap((res: any) => this.total = res.total),
+        map((res: any) => res.results)
+      );
+  }
 
   onSearch() {
     const fields = 'name,description,version,homepage';
